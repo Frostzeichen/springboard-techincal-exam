81c182af911ed5adf10eb03109f353cc37596a89
Frostzeichen <cruztjomar@gmail.com>
2024-05-10 17:10:13 +0800

    fix: incorrect footer placement

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,4 +1,3 @@
-import Footer from '@/Components/Footer';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
@@ -40,7 +39,31 @@ export default function Dashboard({ auth }) {
                     margin-bottom: 20px;
                 }
             `}</style>
-            <Footer position="relative" />
+            <footer>
+                <p><span>Size</span> | Made by Terenz Jomar Dela Cruz &copy; 2024.</p>
+                <style>{`
+                    footer {
+                        position: absolute;
+                        bottom: 0px;
+                    }
+
+                    footer > p > span::before {
+                        content: "PC ";
+                    }
+
+                    @media only screen and (max-width: 1280px) {
+                        footer > p > span::before {
+                            content: "Tablet ";
+                        }
+                    }
+
+                    @media only screen and (max-width: 768px) {
+                        footer > p > span::before {
+                            content: "Mobile ";
+                        }
+                    }
+                `}</style>
+            </footer>
         </AuthenticatedLayout>
     );
 }
