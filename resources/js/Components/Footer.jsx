322619bb94ca1339c875a7c5b158681a8f8ff947
Frostzeichen <cruztjomar@gmail.com>
2024-05-10 11:27:59 +0800

    feat: add footer component

---
+++ resources/js/Components/Footer.jsx
@@ -0,0 +1,29 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p><span>Size</span> | Made by Terenz Jomar Dela Cruz &copy; 2024.</p>
+            <style>{`
+                footer {
+                    position: absolute;
+                    bottom: 0px;
+                }
+
+                footer > p > span::before {
+                    content: "PC ";
+                }
+
+                @media only screen and (max-width: 1280px) {
+                    footer > p > span::before {
+                        content: "Tablet ";
+                    }
+                }
+
+                @media only screen and (max-width: 768px) {
+                    footer > p > span::before {
+                        content: "Mobile ";
+                    }
+                }
+            `}</style>
+        </footer>
+    )
+}
